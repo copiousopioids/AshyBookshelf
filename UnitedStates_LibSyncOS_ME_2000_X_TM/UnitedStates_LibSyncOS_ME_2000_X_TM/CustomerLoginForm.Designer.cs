de0acdf96193182d3da8ccd367ca32c96ddbb349
Austen Henry <austen@WIN2.CS.KSU.EDU>
2017-11-18 16:08:23 -0600

    Added customer login form and customer home form.

---
+++ UnitedStates_LibSyncOS_ME_2000_X_TM/UnitedStates_LibSyncOS_ME_2000_X_TM/CustomerLoginForm.Designer.cs
@@ -0,0 +1,38 @@
+﻿namespace UnitedStates_LibSyncOS_ME_2000_X_TM
+{
+    partial class CustomerLoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Text = "CustomerLoginForm";
+        }
+
+        #endregion
+    }
+}
