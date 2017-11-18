750e1e7f0d0d7ecab3ab46618b4092e875279706
Reagan Wood <reaganwood1@WIN2.CS.KSU.EDU>
2017-11-18 16:15:26 -0600

    Added StaffAddBookItemWindow

--- UnitedStates_LibSyncOS_ME_2000_X_TM/UnitedStates_LibSyncOS_ME_2000_X_TM/StaffItemSearchWindow.Designer.cs
+++ UnitedStates_LibSyncOS_ME_2000_X_TM/UnitedStates_LibSyncOS_ME_2000_X_TM/StaffItemSearchWindow.Designer.cs
@@ -36,6 +36,7 @@
             this.button3 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -69,16 +70,16 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 23);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Add Customer";
+            this.button1.Text = "Add Book";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(205, 215);
+            this.button2.Location = new System.Drawing.Point(282, 215);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 23);
             this.button2.TabIndex = 4;
-            this.button2.Text = "Delete Customer";
+            this.button2.Text = "Delete Item";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
@@ -110,11 +111,21 @@
             this.checkBox2.Text = "Search Movies";
             this.checkBox2.UseVisualStyleBackColor = true;
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(112, 215);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Add Movie";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
             // StaffItemSearchWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(491, 261);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button3);
@@ -140,5 +151,6 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button4;
     }
 }
