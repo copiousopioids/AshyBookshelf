bc3752425605b280492f75e68f223e94eaaf3e91
Reagan Wood <reaganwood1@WIN2.CS.KSU.EDU>
2017-11-19 14:15:06 -0600

    Added Item Entities

---
+++ UnitedStates_LibSyncOS_ME_2000_X_TM/UnitedStates_LibSyncOS_ME_2000_X_TM/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitedStates_LibSyncOS_ME_2000_X_TM
+{
+    public class Book : Item
+    {
+        public Book(Condition condition, bool available, int damageFine, Genre genre, int id, string title, int weeklyFine, int isbn, string publisher, int numberOfPages) {
+            this.Isbn = isbn;
+            this.Publisher = publisher;
+            this.NumberOfPages = numberOfPages;
+            this.Available = available;
+            this.Condition = condition;
+            this.DamageFine = DamageFine;
+            this.Genre = genre;
+            this.ID = id;
+            this.Title = title;
+            this.WeeklyFine = weeklyFine;
+        }
+
+        public int Isbn { get; set; }
+        public string Publisher { get; set; }
+        public int NumberOfPages { get; set; }
+
+        public bool Available { get; set; }
+
+
+        public Condition Condition { get; set; }
+
+        public int DamageFine { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public int WeeklyFine { get; set; }
+    }
+}
---
+++ UnitedStates_LibSyncOS_ME_2000_X_TM/UnitedStates_LibSyncOS_ME_2000_X_TM/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitedStates_LibSyncOS_ME_2000_X_TM
+{
+    public interface Item
+    {
+        string Title { get; set; }
+        bool Available { get; set; }
+        int WeeklyFine { get; set; }
+        int DamageFine { get; set; }
+        Condition Condition { get; set; }
+        Genre Genre { get; set; }
+        int ID { get; set; }
+        // TODO: IMPLEMENT PEOPLE ON MERGE
+    }
+}
