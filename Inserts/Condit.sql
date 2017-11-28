981eb4524dbd2a778bab661d4e719b7e957f16ba
Justin Coen <30835879+jhcoen28@users.noreply.github.com>
2017-11-28 12:08:38 -0600

    Insert files

---
+++ Inserts/Condit.sql
@@ -0,0 +1,4 @@
+INSERT INTO Condit(code, condition) VALUES(1,'Excellent');
+INSERT INTO Condit(code, condition) VALUES(2, 'Good');
+INSERT INTO Condit(code, condition) VALUES(3, 'Fair');
+INSERT INTO Condit(code, condition) VALUES(4, 'Poor');
---
+++ Inserts/Roles.sql
@@ -0,0 +1,3 @@
+INSERT INTO Roles(role_code,role) VALUES(1, Actor);
+INSERT INTO Roles(role_code,role) VALUES(2, Director);
+INSERT INTO Roles(role_code,role) VALUES(3, Author);
