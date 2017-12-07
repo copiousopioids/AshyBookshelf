ba157f1218d1c3d379f6379c3504fd1d70e9609b
Justin Coen <30835879+jhcoen28@users.noreply.github.com>
2017-12-07 16:26:15 -0600

    Added Fine inserts

---
+++ Inserts/Fines.sql
@@ -0,0 +1,8 @@
+INSERT INTO `Fines` (`fine_id`, `amount`, `due_date`, `paid`, `description`) VALUES
+(15, 5, '2017-11-28', b'1', 'Weekly Fine'),
+(16, 10, '2017-11-28', b'0', 'Damage Fine'),
+(17, 10, '2017-11-29', b'0', 'Damage Fine'),
+(18, 5, '2017-12-13', b'0', 'Weekly Fine'),
+(19, 10, '2017-12-13', b'1', 'Damage Fine'),
+(20, 5, '2017-12-13', b'1', 'Weekly Fine'),
+(21, 10, '2017-12-13', b'1', 'Damage Fine');
